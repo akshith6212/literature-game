eb3b2d581c8574006e24262e1dacc306811732af
akshithananthula <akshith6212@gmail.com>
2026-04-02 00:04:57 +0530

    updated firebase config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const FIREBASE_CONFIG = {
-  apiKey: "Enter your API key here",
-  authDomain: "Enter your Auth Domain here",
-  databaseURL: "Enter your Database URL here",
-  projectId: "Enter your Project ID here",
-  storageBucket: "Enter your Storage Bucket here",
-  messagingSenderId: "Enter your Messaging Sender ID here",
-  appId: "Enter your App ID here",
+  apiKey: "AIzaSyCIjnd5ix39Ocp0bJUfZt81b0Bm1Evcdqw",
+  authDomain: "literature-game-a1da8.firebaseapp.com",
+  databaseURL: "https://literature-game-a1da8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "literature-game-a1da8",
+  storageBucket: "literature-game-a1da8.firebasestorage.app",
+  messagingSenderId: "1069416573230",
+  appId: "1:1069416573230:web:1360cbeabc7047668bf2bf",
 };
 
 export default FIREBASE_CONFIG;
