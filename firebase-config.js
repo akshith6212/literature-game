5911ab2935853ffcab7de1656191b250436101c3
akshithananthula <akshith6212@gmail.com>
2026-04-02 00:01:47 +0530

    Added firebase config

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+  apiKey: "Enter your API key here",
+  authDomain: "Enter your Auth Domain here",
+  databaseURL: "Enter your Database URL here",
+  projectId: "Enter your Project ID here",
+  storageBucket: "Enter your Storage Bucket here",
+  messagingSenderId: "Enter your Messaging Sender ID here",
+  appId: "Enter your App ID here",
+};
+
+export default FIREBASE_CONFIG;
